f8651eeade43878a230efc2cf1ad056e2d01af32
Christopher Wozniak <cwozniak@aboutstaffing.com>
2018-10-18 11:39:11 -0600

    Added inspec tests
    Default Recipe configures the runtime zone to be work on the primary 
    wired interface (the first anyways)

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,9 @@
-#
-# Cookbook:: firewall-cookbook
-# Recipe:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
+package 'firewalld'
+
+service 'firewalld' do
+  action [:start, :enable]
+end
+
+execute 'runtime_zone_add' do
+  command 'sudo firewall-cmd --zone=work --change-interface=`ls /sys/class/net | grep e`'
+end
